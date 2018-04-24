f46a2d0acce02f3b123110aeec01f5457a4386ea
Chris Crowder <chris.crowder.95@gmail.com>
2018-04-24 13:32:03 -0400

    updated bootstrap cdn, previous one did not work correctly on mobile devices

--- index.php
+++ index.php
@@ -3,19 +3,26 @@
 <html>
 <head>
 
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
   <title>Homepage</title>
 
-  <!-- Bootstrap CSS CDN -->
-  <link rel="stylesheet"
-    href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
-    integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
-    crossorigin="anonymous">
-  
-  <!-- Font -->
-  <link href="https://fonts.googleapis.com/css?family=Exo+2" rel="stylesheet">
-  
-  <!-- Customized Style -->
-  <link rel = 'stylesheet' href = 'home-style.css'>
+  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+
+  <!-- Latest compiled and minified CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+  <!-- Optional theme -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+  <!-- Latest compiled and minified JavaScript -->
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+  <link rel="stylesheet" href="home-style.css">
 
 </head>
 
@@ -37,14 +44,17 @@
 
     <div class = "col-sm-0 col-md-3" id="left-margin">
 
-      <button id="submit-post-btn" onclick="showSubmitForm()">SUBMIT NEW POST</button>
+      <button class="btn" id="submit-post-btn" onclick="showSubmitForm()">SUBMIT NEW POST</button>
 
       <form id="submit-form" action="submit_post.php" method="post">
         <p><b>Message:</b></p>
-        <textarea name="msg" rows="5" cols="40" maxlength="200"></textarea>
+        <div class="container-fluid">
+          <textarea class="form-control" name="msg" rows="5" cols="40" maxlength="200"></textarea>
+        </div>
+        
         <br>
-        <button type="submit"> Submit Post </button>
         <hr>
+        <button class="btn btn-primary" type="submit"> Submit Post </button>
       </form>
 
     </div>
