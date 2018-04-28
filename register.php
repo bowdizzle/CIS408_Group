c2a0e629d2685337eebdd35377bdd79986c1eab2
Chris Crowder <chris.crowder.95@gmail.com>
2018-04-27 20:29:08 -0400

    Updated register page to use alerts instead of a redirect

--- register.php
+++ register.php
@@ -1,35 +1,67 @@
 <!DOCTYPE html>
 <html>
 <head>
+
+	<!-- Required meta tags -->
 	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-	<title>Register</title>
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
-	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	<!-- Optional JavaScript -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
-	<!-- Latest compiled and minified CSS -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<!-- Font and Style -->
+	<link href="https://fonts.googleapis.com/css?family=Exo+2" rel="stylesheet">
 
-	<!-- Optional theme -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-
-	<!-- Latest compiled and minified JavaScript -->
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+	<!--ANGULAR-->
+	<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.4/angular.min.js">
+	</script>
 
 	<link rel="stylesheet" href="home-style.css">
+
 </head>
 <body>
 
+	<script type="text/javascript">
+		$(function () {
+			$('#register-form').submit(function (event) {
+
+				event.preventDefault();
+				let formData = $('#register-form').serialize();
+
+				$.ajax({
+
+					type: 'POST',
+					url: 'register_action.php',
+					data: formData
+
+				}).done(response => {
+					$('#message-alert').show();
+					$('#message-alert').text(response);
+					$("#message-alert").fadeTo(3000, 500).slideUp(500, function () {
+						$("#message-alert").slideUp(500);
+					});
+
+				});
+			});
+		});
+	</script>
+
 	<div class='jumbotron' id='site-header'>
-    	<h1 id='site-title'> <a href=index.php>NAME TBD </a></h1>
+    	<h1 id='site-title'> <a href=index.php>NAME TBD</a></h1>
  	</div>
 
 	<div class="container">
-		<form action="register_action.php" method="post" role="form">
+		
+		<div id="message-alert" class="alert alert-dark alert-dismissable" role="alert" style="display: none;">
+		  Message alert!
+		</div>
+		
+		<form id="register-form" action="register_action.php" method="post" role="form">
 			<div class="form-group">
 				<label for="username">Username:</label>
 				<input type="text" class="form-control" id="username" name="username" required>
@@ -43,7 +75,7 @@
 				<input type="password" class="form-control" id="pwd_confirm" required>
 			</div>
 			<button type="submit" class="btn btn-primary">Submit</button>
-			<button type="button" class="btn btn-default" action="/index.php">Cancel</button>
+			<button type="button" class="btn btn-default" action="index.php">Cancel</button>
 		</form>
 	</div>
 
--- register_action.php
+++ register_action.php
@@ -1,88 +1,51 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+<?php
 
-	<title>Register</title>
+// initialize the ini file 
+$ini = parse_ini_file("config.ini");
 
-	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+// set up connection and statement to prevent sql injection
+$mysqli = new mysqli($ini["db_ip"], $ini["db_user"], $ini["db_password"]);
+$mysqli->set_charset("utf8mb4");
 
-	<!-- Latest compiled and minified CSS -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+if (mysqli_connect_errno()) {
+	exit();
+}
 
-	<!-- Optional theme -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-
-	<!-- Latest compiled and minified JavaScript -->
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-
-	<link rel="stylesheet" href="home-style.css">
-</head>
-<body>
-
-	<div class='jumbotron' id='site-header'>
-    	<h1 id='site-title'> <a href=index.php>NAME TBD </a></h1>
-	</div>
+// query strings
+$db_create = "CREATE DATABASE IF NOT EXISTS " . $ini["db_name"];
+$table_create = "CREATE TABLE IF NOT EXISTS accounts (
+	id INT(10) AUTO_INCREMENT,
+	username VARCHAR(36) NOT NULL,
+	password VARCHAR(255) NOT NULL,
+	PRIMARY KEY (id),
+	UNIQUE (username)
+	)";
 	
-	<div class="container">
-		
-		<?php
+// creates database if it doesnt exist
+$mysqli->query($db_create);
+$mysqli->select_db($ini["db_name"]);
 
-		$ini = parse_ini_file("config.ini");
+// creates table if it doesnt exist
+$mysqli->query($table_create);
 
-		// set up connection and statement to prevent sql injection
-		$mysqli = new mysqli($ini["db_ip"], $ini["db_user"], $ini["db_password"]);
-		$mysqli->set_charset("utf8mb4");
+// prepare insert statement to add account
+$username = $_POST["username"];
+$password = password_hash($_POST["password"], PASSWORD_DEFAULT);
 
-		if (mysqli_connect_errno()) {
-			echo "Failed to connect to MySQL: " . mysqli_connect_error();
-			exit();
-		}
+$stmt = $mysqli->prepare("INSERT INTO accounts (username, password) VALUES (?,?)");
+$stmt->bind_param('ss', $username, $password);
 
-		// query strings
-		$db_create = "CREATE DATABASE IF NOT EXISTS " . $ini["db_name"];
-		$table_create = "CREATE TABLE IF NOT EXISTS accounts (
-							id INT(10) AUTO_INCREMENT,
-							username VARCHAR(36) NOT NULL,
-							password VARCHAR(255) NOT NULL,
-							PRIMARY KEY (id),
-							UNIQUE (username)
-						)";
 
-		// creates database if it doesnt exist
-		$mysqli->query($db_create);
-		$mysqli->select_db($ini["db_name"]);
+// perform the statement and output result
+if ($stmt->execute()) {
+	echo "You have succesfully registered as " . $username . "!";
+} else {
+	echo "Username has been taken, please try another username.";
+}
 
-		// creates table if it doesnt exist
-		$mysqli->query($table_create);
+// clean up
+$stmt->free_result();
+$stmt->close();
+$mysqli->close();
 
-		// prepare insert statement to add account
-		$username = $_POST["username"];
-		$password = password_hash($_POST["password"], PASSWORD_DEFAULT);
-
-		$stmt = $mysqli->prepare("INSERT INTO accounts (username, password) VALUES (?,?)");
-		$stmt->bind_param('ss', $username, $password);
-		
-		if ($stmt->execute()) {
-			echo "You have succesfully registered as " . $username . "!";
-		} else {
-			echo "Username has been taken, please try another username.";
-		}
-
-		// clean up
-		$stmt->free_result();
-		$stmt->close();
-		$mysqli->close();
-		
-		 ?>
-
-	</div>
-	
-	<php>
-
-</body>
-</html>
+?>
